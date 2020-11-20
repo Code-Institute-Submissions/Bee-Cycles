e8c9b9b6acbb51be1731f05441a0ef45142f5f47
todorr92 <todorovicslobodan10@gmail.com>
2020-11-20 14:03:47 +0000

    Removed featured products, will add them once I have everything ready

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404 
 from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
