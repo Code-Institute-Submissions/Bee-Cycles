ec0aef3e4e1669a78f61b0f0a3741bb2af88b8b0
todorr92 <todorovicslobodan10@gmail.com>
2020-11-30 13:42:09 +0000

    Fixed typos in city fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,7 +22,7 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'country': 'Country',
             'postcode': 'Postal Code',
-            'city': 'Town or City',
+            'city': 'City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
--- checkout/views.py
+++ checkout/views.py
@@ -23,7 +23,7 @@ def checkout(request):
             'phone_number': request.POST['phone_number'],
             'country': request.POST['country'],
             'postcode': request.POST['postcode'],
-            'city': request.POST['town_or_city'],
+            'city': request.POST['city'],
             'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
