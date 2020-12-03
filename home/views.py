d2185a70f31cbad229737d9e2e88337dcca94d32
todorr92 <todorovicslobodan10@gmail.com>
2020-12-03 19:47:07 +0000

    Added filtered products to index.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return index.html """
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'home/index.html', context)
