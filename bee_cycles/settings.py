1d151e4bbf9738d68cc9d38b2530ea81e210dac5
todorr92 <todorovicslobodan10@gmail.com>
2020-12-07 09:38:33 +0000

    Added bee-cycles.heroku as allowed host

--- bee_cycles/settings.py
+++ bee_cycles/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@$zoi9gx7_1ou@-y_(&7l=m$@m2t-c3fw++e-4s%z42ab%9+d5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bee-cycles.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,13 +118,17 @@ WSGI_APPLICATION = 'bee_cycles.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL':
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
